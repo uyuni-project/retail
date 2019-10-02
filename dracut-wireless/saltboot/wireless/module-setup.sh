d0c13c2d5f73e524c19dc1efdd0b48f59aad0248
Vladimir Nadvornik <nadvornik@suse.cz>
2019-10-02 15:54:18 +0200

    Allow use of different ifcfg in initrd

--- dracut-wireless/saltboot/wireless/module-setup.sh
+++ dracut-wireless/saltboot/wireless/module-setup.sh
@@ -22,9 +22,18 @@ install() {
     inst_rules "$moddir/80-wireless.rules"
     inst_script "$moddir/wireless_up.sh" "/sbin/wireless_up"
 
-    #include wlan configuration for wifi credentials, even in no-hostonly mode
-    inst_multiple -o /etc/sysconfig/network/ifcfg-wlan*
-    
+    # include wlan configuration for wifi credentials, even in no-hostonly mode
+    # if there is initrd-ifcfg-wlan*, prefer it over ifcfg-wlan*
+    for f in /etc/sysconfig/network/initrd-ifcfg-wlan* ; do
+        [ ! -f "$f" ] && continue
+        inst "$f" "${f/initrd-/}"
+    done
+    for f in /etc/sysconfig/network/ifcfg-wlan* ; do
+        [ ! -f "$f" ] && continue
+        [ -f "${f/ifcfg-/initrd-ifcfg-/}" ] && continue
+        inst "$f"
+    done
+
     inst_hook pre-pivot 99 "$moddir/wireless_update.sh"
     inst_hook cleanup 99 "$moddir/wireless_cleanup.sh"
 }
