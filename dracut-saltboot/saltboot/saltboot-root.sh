23675c4a547d47a6f84c0e72557ba86026d252a9
Ondrej Holecek <oholecek@suse.com>
2020-04-09 14:01:17 +0200

    Add missing naming modifiers as exported veriables
    Added USE_FQDN_MINION_ID, DISABLE_HOSTNAME_ID and DISABLE_ID_PREFIX
    to exported variables.

--- dracut-saltboot/saltboot/saltboot-root.sh
+++ dracut-saltboot/saltboot/saltboot-root.sh
@@ -30,9 +30,17 @@ if [ -z "$server" ] ; then
     DHCPORSERVER="1"
 fi;
 
+# Salt minion modifiers
 export MASTER=$(getarg MASTER=)
 export MINION_ID_PREFIX=$(getarg MINION_ID_PREFIX=)
-export DISABLE_UNIQUE_SUFFIX=$(getarg DISABLE_UNIQUE_SUFFIX=)
 export SALT_TIMEOUT=$(getarg SALT_TIMEOUT=)
 export salt_device=$(getarg salt_device=)
+
+# Terminal naming modifiers
+export DISABLE_UNIQUE_SUFFIX=$(getarg DISABLE_UNIQUE_SUFFIX=)
+export DISABLE_HOSTNAME_ID=$(getarg DISABLE_HOSTNAME_ID=)
+export DISABLE_ID_PREFIX=$(getarg DISABLE_ID_PREFIX=)
+export USE_FQDN_MINION_ID=$(getarg USE_FQDN_MINION_ID=)
+
+# Debugging
 export kiwidebug=$(getarg kiwidebug=)
