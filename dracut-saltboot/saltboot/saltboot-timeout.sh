3fe82b7c47049aa0a24af82018815467f36b524a
Ondrej Holecek <oholecek@suse.com>
2020-08-13 12:06:14 +0200

    Separate timeout script from saltboot-root

---
+++ dracut-saltboot/saltboot/saltboot-timeout.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
+
+# saltboot timeout script
+# if $root is specified, usually means terminal is already deployed and thus network is optional
+# wait for either network or device to boot from to be ready
+
+if [ -z "$root" ] || [ "$root"x == "saltbootx" ]; then
+  exit 0
+fi
+
+_name="$(str_replace "${root#block:}" '/' '\x2f')"
+for f in wait-network.sh "devexists-$_name.sh"; do
+  if [ ! -e "$hookdir/initqueue/finished/\$f" ] || ( . "$hookdir/initqueue/finished/\$f" ); then
+    rm -f -- "$hookdir/initqueue/finished/wait-network.sh"
+    rm -f -- "$hookdir/initqueue/finished/devexists-$_name.sh"
+  fi
+done
