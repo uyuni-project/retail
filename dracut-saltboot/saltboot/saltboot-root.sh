80ee6ce96e185148432a55cfc876f8432c764902
Ondrej Holecek <oholecek@suse.com>
2025-06-26 15:47:39 +0200

    Introduce new rd.saltboot namespace
    This commit introduces new rd.saltboot cmdline namespace:
    
    String values:
    MASTER => rd.saltboot.master
    MINION_ID_PREFIX => rd.saltboot.id_prefix
    SALT_TIMEOUT => rd.saltboot.timeout
    SALT_STOP_TIMEOUT => rd.saltboot.stop_timout
    salt_device => rd.saltboot.device
    
    Boolean values, now only set if true:
    DISABLE_UNIQUE_SUFFIX => rd.saltboot.nosuffix
    DISABLE_HOSTNAME_ID => rd.saltboot.nohostname
    DISABLE_ID_PREFIX => rd.saltboot.noprefix
    USE_FQDN_MINION_ID => rd.saltboot.usefqdn
    USE_MAC_MINION_ID => rd.saltboot.usemac
    kiwidebug => rd.saltboot.debug

--- dracut-saltboot/saltboot/saltboot-root.sh
+++ dracut-saltboot/saltboot/saltboot-root.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+command -v getarg > /dev/null || . /lib/dracut-lib.sh
+
 if [ -z "$root" ] ; then
   root=saltboot
 fi
@@ -8,22 +10,40 @@ rootok=1
 
 # If we don't have a server, we need dhcp
 if [ -z "$server" ] ; then
-    DHCPORSERVER="1"
+  DHCPORSERVER="1"
 fi;
 
+# Saltboot prefix
+_prefix="rd.saltboot"
+
 # Salt minion modifiers
-export MASTER=$(getarg MASTER=)
-export MINION_ID_PREFIX=$(getarg MINION_ID_PREFIX=)
-export SALT_TIMEOUT=$(getarg SALT_TIMEOUT=)
-export SALT_STOP_TIMEOUT=$(getarg SALT_STOP_TIMEOUT=)
-export salt_device=$(getarg salt_device=)
+MASTER=$(getarg "${_prefix}.master=" -d -y "MASTER=")
+export MASTER
+
+MINION_ID_PREFIX=$(getarg "${_prefix}.id_prefix=" -d -y "MINION_ID_PREFIX=")
+export MINION_ID_PREFIX
+
+SALT_TIMEOUT=$(getargnum 60 10 3600 "${_prefix}.timeout=" -d -y "SALT_TIMEOUT=")
+export SALT_TIMEOUT
+
+SALT_STOP_TIMEOUT=$(getargnum 15 1 60 "${_prefix}.stop_timeout=" -d -y "SALT_STOP_TIMEOUT=")
+export SALT_STOP_TIMEOUT
+
+salt_device=$(getarg "${_prefix}.device=" -d -y "salt_device")
+export salt_device
 
 # Terminal naming modifiers
-export DISABLE_UNIQUE_SUFFIX=$(getarg DISABLE_UNIQUE_SUFFIX=)
-export DISABLE_HOSTNAME_ID=$(getarg DISABLE_HOSTNAME_ID=)
-export DISABLE_ID_PREFIX=$(getarg DISABLE_ID_PREFIX=)
-export USE_FQDN_MINION_ID=$(getarg USE_FQDN_MINION_ID=)
-export USE_MAC_MINION_ID=$(getarg USE_MAC_MINION_ID=)
+getargbool 0 "${_prefix}.nosuffix" -d -y "DISABLE_UNIQUE_SUFFIX=" && DISABLE_UNIQUE_SUFFIX=1
+export DISABLE_UNIQUE_SUFFIX
+getargbool 0 "${_prefix}.nohostname" -d -y "DISABLE_HOSTNAME_ID=" && DISABLE_HOSTNAME_ID=1
+export DISABLE_HOSTNAME_ID
+getargbool 0 "${_prefix}.noprefix" -d -y "DISABLE_ID_PREFIX=" && DISABLE_ID_PREFIX=1
+export DISABLE_ID_PREFIX
+getargbool 0 "${_prefix}.usefqdn" -d -y "USE_FQDN_MINION_ID=" && USE_FQDN_MINION_ID=1
+export USE_FQDN_MINION_ID
+getargbool 0 "${_prefix}.usemac" -d -y "USE_MAC_MINION_ID=" && USE_MAC_MINION_ID=1
+export USE_MAC_MINION_ID
 
 # Debugging
-export kiwidebug=$(getarg kiwidebug=)
+getargbool 0 "${_prefix}.debug=" -d -y "kiwidebug=" && kiwidebug=1
+export kiwidebug
